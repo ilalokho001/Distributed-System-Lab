fbb20a6f5bab59bb1c4b7afa8b6c38256b1add25
ilalokho001 <42623137+ilalokho001@users.noreply.github.com>
2021-07-10 15:11:22 +0200

    Delete hse-2021-summer-main/hse-2021-summer-main directory

--- hse-2021-summer-main/hse-2021-summer-main/code/TodoListSpringBootRestRepository/src/main/java/de/maeddes/TodoListSpringBootRestRepository/TodoRestRepository.java
+++
@@ -1,9 +0,0 @@
-package de.maeddes.TodoListSpringBootRestRepository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(collectionResourceRel = "todos", path = "todos-hal")
-public interface TodoRestRepository extends CrudRepository<Todo,String>{
-    
-}
--- hse-2021-summer-main/hse-2021-summer-main/code/persistenceExample/src/main/java/com/example/persistenceExample/StringRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.persistenceExample;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface StringRepository extends CrudRepository<StringEntity, String>{
-    
-}
